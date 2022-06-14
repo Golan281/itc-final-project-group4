f9e222d9ec2553f3fdf27608b7609861e631eabd
ValeChH <93483575+ValeCHH@users.noreply.github.com>
2022-06-14 22:22:11 +0300

    Doing something

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import Home from "./components/pages/Home";
+import Navbar from "./components/elements/Navbar";
+import AppContext from "../src/components/Context/userContext";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState();
+  const token = JSON.parse(localStorage.getItem("token"));
+  const [allSaveByUser, setAllSaveByUser] = useState();
+  const [newTab, setNewTab] = useState();
+  const [newWorkspace, setNewWorksapce] = useState();
+
+  useEffect(() => {
+    async function getUser() {
+      try {
+        if (!token) {
+          return;
+        }
+        const decoded = jwt_decode(token);
+        const res = await axios.get(``);
+        setCurrentUser(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getUser();
+  }, []);
+
+  useEffect(() => {
+    async function getUserWorkSpace() {
+      const headersConfig = {
+        Authorization: `Bearer ${token}`,
+      };
+      try {
+        const res = await axios.get(``, {
+          headers: headersConfig,
+        });
+
+        setAllSaveByUser(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getUserWorkSpace();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider
+      value={{
+        currentUser,
+        allSaveByUser,
+        setAllSaveByUser,
+        newWorkspace,
+        setNewWorksapce,
+        newTab,
+        setNewTab,
+      }}
+    >
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
---
+++ src/components/elements/Navbar.js
@@ -0,0 +1,215 @@
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Modal from "react-modal";
+import "./Navbar.css";
+import userContext from "../Context/userContext";
+import axios from "axios";
+import logo from "../images/Logo.PNG"
+
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "70%",
+  },
+};
+
+Modal.setAppElement("#root");
+
+function Navbar() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({
+    first: "",
+    last: "",
+    email: "",
+    password: "",
+    repassword: "",
+    phone: "",
+  });
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const { addUser, setCurrentUser, currentUser } = useContext(userContext);
+  const navigate = useNavigate();
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const handleChangeSignUp = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitLogin = async (e) => {
+    e.preventDefault();
+    const user = {
+      email: email,
+      password: password,
+    };
+
+    const res = await axios.post("", user);
+    if (res.data.token) {
+      setEmail("");
+      setPassword("");
+      localStorage.setItem("token", JSON.stringify(res.data.token));
+      setCurrentUser(res.data.user);
+      navigate("/");
+      closeModal();
+    }
+  };
+
+  const onSubmitSignUp = async (e) => {
+    try {
+      e.preventDefault();
+      const res = await axios.post("", user);
+      if (res.data) {
+        setUser({
+          first: "",
+          last: "",
+          email: "",
+          password: "",
+          admin: "",
+        });
+
+        addUser({ ...user });
+        navigate("/");
+        closeModal();
+        alert("Thanks for signing up. Please Log in.");
+      }
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const HandleLogOut = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+    window.location.reload();
+  };
+
+  return (
+    <div className="navbar-wrapper">
+      <div className="navbar-left">
+        <img
+          src={logo}
+          height="50"
+          className="navbar-item"
+          alt="logo"
+        ></img>
+        
+</div>
+<div className="navbar-right">
+        {currentUser ? (
+          <div>
+            <Link className="navbar-item" to="/">
+              Home
+            </Link>
+          </div>
+        ) : (
+          <div></div>
+        )}
+        {!currentUser ? (
+          <div className="signup button" onClick={openModal}>
+            Sign up/ Log in
+          </div>
+        ) : (
+          <div className="signup button" onClick={HandleLogOut}>
+            Sign out
+          </div>
+        )}
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <div className="modal-form">
+          <div className="modal-form-size">
+            <img
+              src={logo}
+              width="300"
+              className="navbar-item"
+              alt="logo"
+            ></img>
+            <h1>Log In</h1>
+            <div className="form-item">Email</div>
+            <input
+              type="email"
+              className="form-item"
+              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+            />
+            <div className="form-item">Password</div>
+            <input
+              type="password"
+              className="form-item"
+              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+            />
+            <div className="modal-form-button" onClick={onSubmitLogin}>
+              Log in
+            </div>
+          </div>
+          <div className="vline"></div>
+          <div className="modal-form-size">
+            <h1>Sign Up</h1>
+            <div className="form-item">Email</div>
+            <input
+              type="email"
+              name="email"
+              className="form-item"
+              onChange={handleChangeSignUp}
+              id="email"
+            />
+            <div className="form-item">Password</div>
+            <input
+              type="password"
+              name="password"
+              className="form-item"
+              onChange={handleChangeSignUp}
+              id="password"
+            />
+            <div className="form-item">Confirm Password</div>
+            <input
+              type="password"
+              name="repassword"
+              className="form-item"
+              onChange={handleChangeSignUp}
+              id="repassword"
+            />
+            <div className="form-item">First Name</div>
+            <input
+              type="text"
+              name="first"
+              className="form-item"
+              onChange={handleChangeSignUp}
+              id="first"
+            />
+            <div className="form-item">Last Name</div>
+            <input
+              type="text"
+              name="last"
+              className="form-item"
+              onChange={handleChangeSignUp}
+              id="last"
+            />
+            <div className="modal-form-button" onClick={onSubmitSignUp}>
+              Sign Up
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+export default Navbar;
